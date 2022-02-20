5bac50fe0c81dbd7402e8b75671a9e020fe8b2bc
Tishamoni <86648679+Tishamoni@users.noreply.github.com>
2022-02-20 13:23:05 +0600

    Add files via upload

---
+++ Cat.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package animal1;
+
+/**
+ *
+ * @author nntis
+ */
+public class Cat {
+    public static void main(String[] args){
+        Dog d=new Dog();
+    }
+    
+    
+}
---
+++ Dog.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package animal1;
+import animal2.Cow;
+import animal2.Donkey;
+//import java.util.Scanner;
+/**
+ *
+ * @author nntis
+ */
+public class Dog {
+    public static void main(String[] args){
+        Cat c =new Cat();
+        Cow cow=new Cow();
+        Donkey donkey=new Donkey();
+        
+    }
+    
+}
